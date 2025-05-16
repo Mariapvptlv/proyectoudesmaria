f54a4f78133b77df5bd1ce91a56922549e388f1e
Mariapvptlv <mariapvptlv05@gmail.com>
2025-05-16 18:39:51 -0500

    Add files via upload

---
+++ acciones.js
@@ -0,0 +1,23 @@
+$(window).load(function () {
+
+    var altura = $(window).height()
+    mapa()
+
+    function mapa() {
+        $('#mapa').mapplic({
+            source: 'mapa.json',
+            height: altura,
+            animate: true,
+            mapfill: true,
+            sidebar: true,
+            minimap: true,
+            deeplinking: true,
+            fullscreen: true,
+            hovertip: true,
+            developer: false,
+            maxscale: 4
+        });
+    }
+
+    
+});
